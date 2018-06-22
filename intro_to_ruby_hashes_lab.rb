c456c5e85eb945544fb42e6c5edc588b47b3ca0d
Alex Neustein <alex.neustein@gmail.com>
2018-06-22 02:38:37 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,7 +52,9 @@ def monopoly_with_fourth_tier
   {
     :pieces => 4,
     :names => {
-      :reading_railroad => {},
+      :reading_railroad => {
+        
+      },
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
       :shortline => {}
